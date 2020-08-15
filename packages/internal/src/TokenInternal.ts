f92f5af39278c231a3cfb351349234e806c0193a
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2020-08-15 10:18:02 +0200

    Internal token to accept payload

--- packages/internal/src/TokenInternal.ts
+++ packages/internal/src/TokenInternal.ts
@@ -1,7 +1,7 @@
 import {Token} from "@pallad/security-tokens";
 
-export class TokenInternal extends Token {
-    constructor(readonly type: string, readonly randomKey?: string) {
+export class TokenInternal<T = any> extends Token {
+    constructor(readonly type: string, readonly payload?: T) {
         super();
     }
 }
