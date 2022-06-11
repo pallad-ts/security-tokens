a79b22eabcf9a036b40b13f01bbe4ffbc1e496e0
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2022-06-11 18:25:43 +0200

    payload provider for verifier

--- packages/hawk/src/hawkVerification.ts
+++ packages/hawk/src/hawkVerification.ts
@@ -1,10 +1,11 @@
 import {TokenFactory} from "@pallad/security-tokens-http";
 import {HAWKServerEngine} from "./HAWKServerEngine";
 
-export function hawkVerification(engine: HAWKServerEngine, {withPayload, ...options}: hawkVerification.Options): TokenFactory.Rule {
-	return request => {
+export function hawkVerification(engine: HAWKServerEngine, {withPayload, payloadProvider, ...options}: hawkVerification.Options): TokenFactory.Rule {
+	return async request => {
 		if (withPayload) {
-			return engine.verifyWithPayload(request, options);
+			const payload = payloadProvider ? await payloadProvider(request) : undefined;
+			return engine.verifyWithPayload(request, {...options, payload});
 		}
 		return engine.verifyWithoutPayload(request, options);
 	}
@@ -12,6 +13,7 @@ export function hawkVerification(engine: HAWKServerEngine, {withPayload, ...opti
 
 export namespace hawkVerification {
 	export interface Options extends HAWKServerEngine.Options {
-		withPayload: boolean
+		withPayload: boolean,
+		payloadProvider?: (req: TokenFactory.Request) => Promise<Buffer> | Buffer;
 	}
 }
