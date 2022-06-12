a78ae1e013433bff61a03b1ac13ca7a0dd7b61f1
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2022-06-12 07:42:18 +0200

    Allow to override verification token rule

--- packages/hawk/src/hawkVerification.ts
+++ packages/hawk/src/hawkVerification.ts
@@ -1,19 +1,22 @@
 import {TokenFactory} from "@pallad/security-tokens-http";
 import {HAWKServerEngine} from "./HAWKServerEngine";
 
-export function hawkVerification(engine: HAWKServerEngine, {withPayload, payloadProvider, ...options}: hawkVerification.Options): TokenFactory.Rule {
+export function hawkVerification(engine: HAWKServerEngine, options: hawkVerification.Options.Arg): TokenFactory.Rule {
 	return async request => {
-		if (withPayload) {
-			const payload = payloadProvider ? await payloadProvider(request) : undefined;
-			return engine.verifyWithPayload(request, {...options, payload});
+		const {payload, ...restOptions} = options instanceof Function ? await options(request) : options;
+		if (payload) {
+			return engine.verifyWithPayload(request, {...restOptions, payload});
 		}
-		return engine.verifyWithoutPayload(request, options);
+		return engine.verifyWithoutPayload(request, restOptions);
 	}
 }
 
 export namespace hawkVerification {
 	export interface Options extends HAWKServerEngine.Options {
-		withPayload: boolean,
-		payloadProvider?: (req: TokenFactory.Request) => Promise<Buffer> | Buffer;
+		payload?: Buffer;
+	}
+
+	export namespace Options {
+		export type Arg = ((req: TokenFactory.Request) => Options | Promise<Options>) | Options;
 	}
 }
