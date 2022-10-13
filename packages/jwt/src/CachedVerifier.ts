29f52c3cc6ae1652c05d00f00ddff34b9b60dcd5
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2022-10-13 21:23:40 +0200

    Fixed lru-cache related tests

--- packages/jwt/src/CachedVerifier.ts
+++ packages/jwt/src/CachedVerifier.ts
@@ -5,62 +5,61 @@ import {SecurityTokenError} from "@pallad/security-tokens";
 import * as is from 'predicates';
 
 function getCurrentTimestamp() {
-    return Math.floor(Date.now() / 1000);
+	return Math.floor(Date.now() / 1000);
 }
 
 export class CachedVerifier<T = any> {
-    constructor(private options: CachedVerifier.Options<T>) {
+	constructor(private options: CachedVerifier.Options<T>) {
 
-    }
+	}
 
-    async verify(token: string): Promise<T> {
-        const cacheResult = this.options.cache.get(token);
-        if (cacheResult) {
-            return cacheResult.cata(e => {
-                throw e
-            }, x => x);
-        }
+	async verify(token: string): Promise<T> {
+		const cacheResult = this.options.cache.get(token, {allowStale: false});
+		if (cacheResult) {
+			return cacheResult.cata(e => {
+				throw e
+			}, x => x);
+		}
 
-        const result = await Either.fromPromise<T, SecurityTokenError>(
-            this.options.helper.verify(token, this.options.verifyOptions)
-        );
+		const result = await Either.fromPromise<T, SecurityTokenError>(
+			this.options.helper.verify(token, this.options.verifyOptions)
+		);
 
-        if (result.isRight()) {
-            const timestamp = getCurrentTimestamp();
-            const data = result.right() as any;
-            const age = data.exp - timestamp;
-            if (age > 0) {
-                this.options.cache.set(token, result, age)
-            }
-            return result.right();
-        }
+		if (result.isRight()) {
+			const timestamp = getCurrentTimestamp();
+			const data = result.right() as any;
+			const age = data.exp - timestamp;
+			if (age > 0) {
+				this.options.cache.set(token, result, {ttl: age})
+			}
+			return result.right();
+		}
 
-        const shouldCacheError = this.isCacheableError(result.left());
+		const shouldCacheError = this.isCacheableError(result.left());
+		if (shouldCacheError) {
+			this.options.cache.set(token, result);
+		}
+		throw result.left();
+	}
 
-        if (shouldCacheError) {
-            this.options.cache.set(token, result);
-        }
-        throw result.left();
-    }
+	private isCacheableError(err: Error) {
+		const cacheError = this.options.options?.cacheError ?? false;
 
-    private isCacheableError(err: Error) {
-        const cacheError = this.options.options?.cacheError ?? false;
+		if (is.bool(cacheError)) {
+			return cacheError;
+		}
 
-        if (is.bool(cacheError)) {
-            return cacheError;
-        }
-
-        return cacheError(err);
-    }
+		return cacheError(err);
+	}
 }
 
 export namespace CachedVerifier {
-    export interface Options<T> {
-        helper: JWTHelper;
-        cache: LRUCache<string, Either<SecurityTokenError, T>>;
-        verifyOptions: JWTHelper.VerifyOptions,
-        options?: {
-            cacheError?: boolean | ((err: Error) => boolean)
-        }
-    }
+	export interface Options<T> {
+		helper: JWTHelper;
+		cache: LRUCache<string, Either<SecurityTokenError, T>>;
+		verifyOptions: JWTHelper.VerifyOptions,
+		options?: {
+			cacheError?: boolean | ((err: Error) => boolean)
+		}
+	}
 }
--- packages/jwt/test/CachedVerifierTest.ts
+++ packages/jwt/test/CachedVerifierTest.ts
@@ -5,136 +5,107 @@ import LRUCache = require("lru-cache");
 import {Either} from "monet";
 import {SecurityTokenError} from "@pallad/security-tokens";
 import {errors} from "@src/errors";
-import {secret, Secret} from "@pallad/secret";
+import {secret} from "@pallad/secret";
 import * as moment from 'moment';
 
 describe('CachedVerifier', () => {
-    let verifier: CachedVerifier;
-    let helper: JWTHelper;
-    let cache: LRUCache<string, Either<any, SecurityTokenError>>;
+	let verifier: CachedVerifier;
+	let helper: JWTHelper;
+	let cache: LRUCache<string, Either<any, SecurityTokenError>>;
 
-    let timer: sinon.SinonFakeTimers;
+	let timer: sinon.SinonFakeTimers;
 
-    const VERIFY_OPTIONS = {
-        subject: 'any'
-    };
-    const CURRENT_TIMESTAMP = 60;
-    const DURATION = moment.duration(100, 'seconds');
+	const VERIFY_OPTIONS = {
+		subject: 'any'
+	};
+	const CURRENT_TIMESTAMP = 60;
+	const DURATION = moment.duration(100, 'seconds');
 
-    function createVerifier(options?: CachedVerifier.Options<any>['options']) {
-        return new CachedVerifier({
-            helper: helper as any,
-            cache: cache as any,
-            verifyOptions: VERIFY_OPTIONS,
-            options
-        });
-    }
+	function createVerifier(options?: CachedVerifier.Options<any>['options']) {
+		return new CachedVerifier({
+			helper: helper as any,
+			cache: cache as any,
+			verifyOptions: VERIFY_OPTIONS,
+			options
+		});
+	}
 
-    function createData(exp: number) {
-        return {
-            exp,
-            foo: 'bar'
-        };
-    }
+	function createData(exp: number) {
+		return {
+			exp,
+			foo: 'bar'
+		};
+	}
 
-    function createToken(expires: moment.Duration) {
-        return helper.sign({foo: 'bar'}, {
-            subject: 'any',
-            expires
-        });
-    }
+	function createToken(expires: moment.Duration) {
+		return helper.sign({foo: 'bar'}, {
+			subject: 'any',
+			expires
+		});
+	}
 
-    beforeEach(() => {
-        helper = new JWTHelper('HS512', {k1: secret('testPrivateKey')});
-        cache = new LRUCache({
-            max: 10000,
-            maxAge: 10000
-        });
+	beforeEach(() => {
+		helper = new JWTHelper('HS512', {k1: secret('testPrivateKey')});
+		cache = new LRUCache({
+			max: 10000,
+			ttl: 10000,
+			allowStale: false
+		});
 
-        verifier = createVerifier();
-        timer = sinon.useFakeTimers(1000 * CURRENT_TIMESTAMP);
-    });
+		verifier = createVerifier();
+		timer = sinon.useFakeTimers(1000 * CURRENT_TIMESTAMP);
+	});
 
-    afterEach(() => {
-        timer.restore();
-    });
+	afterEach(() => {
+		timer.restore();
+	});
 
-    describe('success result', () => {
-        it('token that still a lot of time to expire', async () => {
-            const token = await createToken(DURATION);
+	describe('error result', () => {
+		it('by default not cached', async () => {
+			const spy = sinon.spy(helper, 'verify');
+			const token = await createToken(moment.duration(100, 'seconds'));
 
-            const data = {
-                iat: CURRENT_TIMESTAMP,
-                exp: CURRENT_TIMESTAMP + 100,
-                foo: 'bar',
-                sub: 'any'
-            };
+			timer.tick(DURATION.asMilliseconds());
 
-            expect(await verifier.verify(token))
-                .toEqual(data);
+			expect(await Either.fromPromise(verifier.verify(token)))
+				.toEqual(Either.Left(errors.EXPIRED()));
 
-            const stub = sinon.stub(helper, 'verify')
-                .throws(new Error('should not be called'));
+			expect(await Either.fromPromise(verifier.verify(token)))
+				.toEqual(Either.Left(errors.EXPIRED()));
 
-            // still returns old data since result is cached
-            expect(await verifier.verify(token))
-                .toEqual(data);
+			sinon.assert.calledTwice(spy);
+		});
 
-            timer.tick(DURATION.asMilliseconds());
+		it('error cache enabled', async () => {
+			const verifier = createVerifier({cacheError: true});
+			const spy = sinon.spy(helper, 'verify');
+			const token = await createToken(moment.duration(100, 'seconds'));
 
-            stub.restore();
-            await expect(verifier.verify(token))
-                .rejects
-                .toThrowError(errors.EXPIRED.defaultMessage);
-        });
-    });
+			timer.tick(DURATION.asMilliseconds());
 
-    describe('error result', () => {
-        it('by default not cached', async () => {
-            const spy = sinon.spy(helper, 'verify');
-            const token = await createToken(moment.duration(100, 'seconds'));
+			expect(await Either.fromPromise(verifier.verify(token)))
+				.toEqual(Either.Left(errors.EXPIRED()));
 
-            timer.tick(DURATION.asMilliseconds());
+			expect(await Either.fromPromise(verifier.verify(token)))
+				.toEqual(Either.Left(errors.EXPIRED()));
 
-            expect(await Either.fromPromise(verifier.verify(token)))
-                .toEqual(Either.Left(errors.EXPIRED()));
+			sinon.assert.calledOnce(spy);
+		});
 
-            expect(await Either.fromPromise(verifier.verify(token)))
-                .toEqual(Either.Left(errors.EXPIRED()));
+		it('error cache disabled', async () => {
+			const verifier = createVerifier({cacheError: false});
+			const spy = sinon.spy(helper, 'verify');
+			const token = await createToken(moment.duration(100, 'seconds'));
 
-            sinon.assert.calledTwice(spy);
-        });
+			timer.tick(DURATION.asMilliseconds());
 
-        it('error cache enabled', async () => {
-            const verifier = createVerifier({cacheError: true});
-            const spy = sinon.spy(helper, 'verify');
-            const token = await createToken(moment.duration(100, 'seconds'));
+			expect(await Either.fromPromise(verifier.verify(token)))
+				.toEqual(Either.Left(errors.EXPIRED()));
 
-            timer.tick(DURATION.asMilliseconds());
+			expect(await Either.fromPromise(verifier.verify(token)))
+				.toEqual(Either.Left(errors.EXPIRED()));
 
-            expect(await Either.fromPromise(verifier.verify(token)))
-                .toEqual(Either.Left(errors.EXPIRED()));
-
-            expect(await Either.fromPromise(verifier.verify(token)))
-                .toEqual(Either.Left(errors.EXPIRED()));
-
-            sinon.assert.calledOnce(spy);
-        });
-
-        it('error cache disabled', async () => {
-            const verifier = createVerifier({cacheError: false});
-            const spy = sinon.spy(helper, 'verify');
-            const token = await createToken(moment.duration(100, 'seconds'));
-
-            timer.tick(DURATION.asMilliseconds());
-
-            expect(await Either.fromPromise(verifier.verify(token)))
-                .toEqual(Either.Left(errors.EXPIRED()));
-
-            expect(await Either.fromPromise(verifier.verify(token)))
-                .toEqual(Either.Left(errors.EXPIRED()));
-
-            sinon.assert.calledTwice(spy);
-        });
-    });
+			sinon.assert.calledTwice(spy);
+		});
+	});
 });
